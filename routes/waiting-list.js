95153bec4d635fe2ce9529347c9b5894ee25b131
Edmond O'Flynn <oflynned@tcd.ie>
2019-08-18 19:22:36 +0200

    add es6 transpilation

--- routes/waiting-list.js
+++ routes/waiting-list.js
@@ -7,6 +7,11 @@ const collection = "waiting-list";
 
 router.post('/', async (req, res) => {
     const {email} = req.body;
+    if (!email) {
+        res.status(400).send();
+        return;
+    }
+
     await createRecord(collection, {email});
     res.status(204).send();
 });
