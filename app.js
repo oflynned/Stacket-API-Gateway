e782e2d73676861c6cde1b78bcdb6d94b518a23a
Edmond O'Flynn <oflynned@tcd.ie>
2019-08-18 18:19:59 +0200

    add cors

--- app.js
+++ app.js
@@ -3,11 +3,15 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require("cors");
 
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/mailing-list');
+const waitingListRouter = require('./routes/waiting-list');
 
 const app = express();
+
+app.use(cors());
+app.options("*", cors());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -20,7 +24,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/mailing-list', usersRouter);
+app.use('/waiting-list', waitingListRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
