53611dfdfda4b93b676c08a42bb0bf6846d74e61
Edmond O'Flynn <oflynned@tcd.ie>
2019-08-18 18:23:19 +0200

    reply with email param

--- routes/waiting-list.js
+++ routes/waiting-list.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.post('/', (req, res) => {
-    res.json({ping: "pong"});
+    res.json({email: req.email || "email"});
 });
 
 module.exports = router;
