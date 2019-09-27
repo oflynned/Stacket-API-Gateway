0bb35717c1257f25d3e597d9db6b57b2139413dd
Edmond O'Flynn <oflynned@tcd.ie>
2019-09-27 12:07:29 +0200

    add base config

---
+++ src/config/index.js
@@ -0,0 +1,13 @@
+const { PORT, ENVIRONMENT, CORS_DOMAIN } = process.env;
+
+const FALLBACK_PORT = 3001;
+
+const isProductionEnvironment = () => ENVIRONMENT === 'production';
+
+const commonConfig = {
+  environment: ENVIRONMENT || 'development',
+  port: parseInt(PORT, 10) || FALLBACK_PORT,
+  corsDomain: (isProductionEnvironment() ? CORS_DOMAIN : '*')
+};
+
+export default commonConfig;
