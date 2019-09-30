69ff4dfa4cce109587e373e92fe8417cbb847650
Edmond O'Flynn <oflynned@tcd.ie>
2019-09-30 15:49:09 +0200

    purged passportjs

--- src/server.js
+++ src/server.js
@@ -1,19 +1,10 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
-import passport from 'passport';
-import uuid from 'node-uuid';
 
 import apollo from './graphql';
 
 import environmentConfig from './config/environmentConfig';
-import authenticationConfig from './config/authenticationConfig';
-import databaseConfig from './config/databaseConfig';
-import { checkAuthorization } from './common/authentication';
-import { createUser } from './controllers/user';
-
-const session = require('express-session');
-const MongoStore = require('connect-mongo')(session);
 
 require('dotenv')
   .config();
@@ -50,52 +41,10 @@ app.use(cors({
 }));
 
 const indexRouter = require('./routes/index');
+const authRouter = require('./routes/authentication');
 
 app.use('/', indexRouter);
-
-app.use(session({
-  store: new MongoStore({ url: databaseConfig.databaseUri }),
-  secret: authenticationConfig.sessionSecret,
-  genid: () => uuid.v4()
-}));
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-app.post(
-  '/me',
-  checkAuthorization,
-  async (req, res) => {
-    if (req.user) {
-      res.status(200)
-        .json(req.user);
-    } else {
-      try {
-        const account = await createUser(req);
-        res.status(201)
-          .json(account);
-      } catch (err) {
-        console.log(err);
-        res.status(400)
-          .json({ error: err.message });
-      }
-    }
-  }
-);
-
-app.post(
-  '/login',
-  passport.authenticate('local'),
-  (req, res) => {
-    res.json(req.user);
-  }
-);
-
-app.get('/logout', (req, res) => {
-  req.logout();
-  res.status(204)
-    .send();
-});
+app.use('/auth', authRouter);
 
 apollo(app);
 
