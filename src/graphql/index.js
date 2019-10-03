6efe6fe3bc9c84b62c5093392fc570d3e95ecfa3
Edmond O'Flynn <oflynned@tcd.ie>
2019-10-03 11:24:30 +0200

    add graphql allow expired sessions ff

--- src/graphql/index.js
+++ src/graphql/index.js
@@ -1,6 +1,7 @@
 import { graphiqlExpress, graphqlExpress } from 'apollo-server-express';
 import { json as parseJson } from 'body-parser';
 
+import { allowExpiredSessions } from '../common/featureFlags';
 import { isDevelopmentEnvironment, isProductionEnvironment } from '../config/environmentConfig';
 import schema from './schema';
 import Session from '../models/session/session';
@@ -13,11 +14,24 @@ const enforceActiveSession = async (req, res, next) => {
 
   const sessionId = req.headers['x-session-id'];
   const session = await Session.findBySessionId(sessionId);
+
+  // if session has already been garbage collected
   if (session === null) {
     return res.status(401)
-      .send();
+      .json({ error: 'session_does_not_exist' });
   }
 
+  // if session has already expired
+  if (!allowExpiredSessions() && session.isExpired()) {
+    return res.status(403)
+      .json({ error: 'session_expired' });
+  }
+
+  // TODO check if IP address matches lookup table of past logins
+  // TODO look into CSRF tokens with sessions
+
+  // session is valid, its ttl should be extended by 8 hours
+  await Session.extendExpiry(sessionId);
   req.user = session.user;
   next();
 };
